ea29716e8bdfda867d892cf6c1e92a12d677dbd3
Jamie Riden <jamie.riden@nccgroup.trust>
2018-12-19 07:46:47 +0000

    build changes

--- distexe.sh
+++ distexe.sh
@@ -2,7 +2,9 @@ rm -rf build
 touch setupexe.py
 cmd /c 'python3.exe setupexe.py build_exe'
 rm -f hashcrackwin.zip
-zip -r hashcrackwin.zip build
+rm -rf hashcrackwin
+mv build/exe.win-amd64-3.6 hashcrackwin
+zip -r hashcrackwin.zip hashcrackwin
+cd hashcrackwin ; cmd /c "hashcrackwin.exe -i kerb7500.txt" ; cd ..
+cd hashcrackwin ; cmd /c "hashcrackwin.exe -i netlmv2.txt" ; cd ..
 rsync --partial --progress hashcrackwin.zip root@zealot:/var/www/html/
-cd build/exe.win-amd64-3.6 ; cmd /c "hashcrackwin.exe -i kerb7500.txt" ; cd ../..
-
